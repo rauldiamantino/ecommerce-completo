37496829a1a6607592ba9e764627b9dac9a1d9a3
Raul Diamantino <rauldiamantino25@gmail.com>
2022-07-20 22:14:28 -0300

    Função JavaScript Abrir Menu Responsivo

---
+++ script.js
@@ -0,0 +1,7 @@
+var menu = document.getElementById('menu-categorias')
+var btnAbrir = document.querySelector('btn-abrir')
+var btnFechar = document.querySelector('body')
+function abrirMenu(){
+    menu.classList.toggle('open')
+    
+}
